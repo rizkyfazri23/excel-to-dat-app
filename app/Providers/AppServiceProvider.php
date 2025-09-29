cecf3a5362bb5dc26cac1f78fe0a6fe0139e73d4
Rizky Fazri <rizky.syahputra@edi-indonesia.co.id>
2025-09-29 13:14:13 +0700

    update serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+        // Paksa skema & root URL sesuai APP_URL
+        if (config('app.url')) {
+            URL::forceRootUrl(config('app.url'));
+        }
+        URL::forceScheme('https');
+    }
     }
 }
